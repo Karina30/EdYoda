eef93fe9b5d60b9dd89e1ed17c9fd4f717bbce9d
Karina30 <63238336+Karina30@users.noreply.github.com>
2023-03-11 22:16:53 +0530

    Add files via upload

---
+++ Assignment3Functions/1Que.py
@@ -0,0 +1,12 @@
+#Write a Python function to sum all the numbers in a list.
+
+
+
+# creating a list
+list1 = [8, 2, 3, 0, 7]
+
+# using sum() function
+total = sum(list1)
+
+# printing total value
+print("Sum of all elements in given list: ", total)
